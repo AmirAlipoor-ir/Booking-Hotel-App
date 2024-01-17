7e3bf1de38d8940475c47a3ed0a6db3120406270
AmirAlipoor-ir <89448149+AmirAlipoor-ir@users.noreply.github.com>
2024-01-17 19:45:41 +0330

    add AppLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header/header";
 import LocationList from "./components/LocationList/LocationList";
 import { Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout/AppLayout";
 
 function App() {
   return (
@@ -11,7 +12,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<LocationList/>}/>
-        {/* <Route path="/" element={<LocationList/>}/> */}
+       <Route path="/hotels" element={<AppLayout/>}>
+<Route index element={<div>hotels</div>}/>
+<Route path=":id" element={<div>single hotel</div>}/>
+       </Route>
       </Routes>
     </div>
   );
---
+++ src/components/AppLayout/AppLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+function AppLayout() {
+  return (
+    <div className="appLayout">
+      <div className="sidebar"><Outlet/></div>
+      <div className="mapContainer">map</div>
+    </div>
+  );
+}
+
+export default AppLayout;
