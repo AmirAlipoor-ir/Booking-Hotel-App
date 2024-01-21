f73bcae059a6a60bd1ac853b3e46006df1eec79b
AmirAlipoor-ir <89448149+AmirAlipoor-ir@users.noreply.github.com>
2024-01-21 21:24:08 +0330

    add getHotel function for currentHotel

--- src/components/context/HotelsProvider.jsx
+++ src/components/context/HotelsProvider.jsx
@@ -1,18 +1,50 @@
-import React, { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
 const HotelContext = createContext();
+const BASE_URL = "http://localhost:5000/hotels";
+
 function HotelsProvider({ children }) {
-  const [searchParams, setSearchparams] = useSearchParams();
+  const [currentHotel, setCurrentHotel] = useState(null);
+  const [isLoadingCurrHotel, setIsLoadinCurrHotel] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
   const destination = searchParams.get("destination");
   const room = JSON.parse(searchParams.get("options"))?.room;
-  const { isLoading, data: hotels } = useFetch(
-    "http://localhost:5000/hotels",
-    `q=${destination}&accommodates_gte=${room || 1}`
-  );
-  return <HotelContext.Provider value={{isLoading,hotels}}>{children}</HotelContext.Provider>;
-}
 
+  const { isLoading, data: hotels } = useFetch(
+    BASE_URL,
+    `q=${destination || ""}&accommodates_gte=${room || 1}`
+  );
+
+  async function getHotel(id) {
+    setIsLoadinCurrHotel(true);
+    try {
+      const { data } = await axios.get(`${BASE_URL}/${id}`);
+      setCurrentHotel(data);
+      setIsLoadinCurrHotel(false);
+    } catch (error) {
+      toast.error(error.message);
+      setIsLoadinCurrHotel(false);
+    }
+  }
+
+  return (
+    <HotelContext.Provider
+      value={{
+        isLoading,
+        hotels,
+        currentHotel,
+        getHotel,
+        isLoadingCurrHotel,
+      }}
+    >
+      {children}
+    </HotelContext.Provider>
+  );
+}
 export default HotelsProvider;
 
 export function useHotels() {
