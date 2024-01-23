b9097b7b8b8a20369053ce758a455eafbdf7290b
AmirAlipoor-ir <89448149+AmirAlipoor-ir@users.noreply.github.com>
2024-01-23 23:52:14 +0330

    add new Routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,28 @@ import AppLayout from "./components/AppLayout/AppLayout";
 import Hotels from "./components/Hotels/Hotels";
 import SingleHotel from "./components/SingleHotel/SingleHotel";
 import HotelsProvider from "./components/context/HotelsProvider";
+import BookmarkLayout from "./components/BookmarkLayout/BookmarkLayout";
+import BookmarkProvider from "./components/context/BookmarkListContext";
 
 function App() {
   return (
-    <HotelsProvider>
-      <Toaster />
-      <Header />
-      <Routes>
-        <Route path="/" element={<LocationList />} />
-        <Route path="/hotels" element={<AppLayout />}>
-          <Route index element={<Hotels/>} />
-          <Route path=":id" element={<SingleHotel/>} />
-        </Route>
-      </Routes>
-    </HotelsProvider>
+    <BookmarkProvider>
+      <HotelsProvider>
+        <Toaster />
+        <Header />
+        <Routes>
+          <Route path="/" element={<LocationList />} />
+          <Route path="/hotels" element={<AppLayout />}>
+            <Route index element={<Hotels />} />
+            <Route path=":id" element={<SingleHotel />} />
+          </Route>
+          <Route path="/bookmark" element={<BookmarkLayout />}>
+            <Route index element={<div>bookmark list </div>} />
+            <Route path="add" element={<div>add new mark</div>} />
+          </Route>
+        </Routes>
+      </HotelsProvider>
+    </BookmarkProvider>
   );
 }
 
