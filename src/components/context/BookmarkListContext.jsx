5dc67f96a7a360024d3ffae5e7b1b71c3fae35a5
AmirAlipoor-ir <89448149+AmirAlipoor-ir@users.noreply.github.com>
2024-01-23 23:49:20 +0330

    add BookmarkContext component

---
+++ src/components/context/BookmarkListContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+const BookmarkContext = createContext();
+const BASE_URL = "http://localhost:5000";
+
+function BookmarkProvider({ children }) {
+  const [currentBookmark, setCurrentBookmark] = useState(null);
+  const [isLoadingCurrBookmark, setIsLoadinCurrBookmark] = useState(false);
+
+  const { isLoading, data: bookmarks } = useFetch( `${BASE_URL}/bookmarks`);
+
+  async function getBookmark(id) {
+    setIsLoadinCurrBookmark(true);
+    try {
+      const { data } = await axios.get(`${BASE_URL}/bookmarks/${id}`);
+      setCurrentBookmark(data);
+      setIsLoadinCurrBookmark(false);
+    } catch (error) {
+      toast.error(error.message);
+      setIsLoadinCurrBookmark(false);
+    }
+  }
+
+  return (
+    <BookmarkContext.Provider
+      value={{
+        isLoading,
+        bookmarks,
+        currentBookmark,
+        getBookmark,
+        isLoadingCurrBookmark,
+      }}
+    >
+      {children}
+    </BookmarkContext.Provider>
+  );
+}
+export default BookmarkProvider;
+
+export function useBookmark() {
+  return useContext(BookmarkContext);
+}
