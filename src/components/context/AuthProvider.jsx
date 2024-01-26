5d3ab717fc9fa89bd80b213090d4898ecf2ecb8b
AmirAlipoor-ir <89448149+AmirAlipoor-ir@users.noreply.github.com>
2024-01-26 23:50:30 +0330

    add AuthProvider context

---
+++ src/components/context/AuthProvider.jsx
@@ -0,0 +1,64 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function authReducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return {
+        user: action.payload,
+        isAuthenticated: true,
+      };
+    case "logout":
+      return {
+        user: null,
+        isAuthenticated: false,
+      };
+    default:
+      throw new Error("Unknown action!");
+  }
+}
+
+const FAKE_USER = {
+  name: "Saheb",
+  email: "user@gmail.com",
+  password: "1234",
+};
+
+export default function AuthProvier({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    authReducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuthenticated,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
